7ee8344081da09550afc128c35b33aec13e4c0e9
Gaurav Singh <112081659+Gauravkazu@users.noreply.github.com>
2022-12-10 14:30:17 +0530

    Add files via upload

---
+++ a/calculator 2.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+    }
+    
+    let number1 = parseFloat(prompt("Enter first number: "));
+    let number2 = parseFloat(prompt("Enter second number: "));
+    
+    let result = add(number1,number2);
+    console.log("The sum is " + result);
+    
+
